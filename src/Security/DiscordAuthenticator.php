e1e4326b6b30e0c3a3ac76d3ad939713d78bf153
Oleksandr Nagal <vt221_nos@student.ztu.edu.ua>
2026-04-24 03:14:57 +0300

    feat: add Discord OAuth2 authenticator

---
+++ src/Security/DiscordAuthenticator.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
+
+class DiscordAuthenticator extends AbstractAuthenticator
+{
+    public function __construct(private RouterInterface $router) {}
+
+    public function supports(Request $request): ?bool
+    {
+        // This authenticator is called manually via UserAuthenticatorInterface
+        return false;
+    }
+
+    public function authenticate(Request $request): Passport
+    {
+        return new SelfValidatingPassport(new UserBadge(''));
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        return new RedirectResponse($this->router->generate('app_dashboard'));
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
+    {
+        return new RedirectResponse($this->router->generate('app_login'));
+    }
+}
